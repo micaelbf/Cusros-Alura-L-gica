423f84580254028edf145277c1d477eb7f14fcc7
micaelbf <micaelbferreira@gmail.com>
2024-08-16 18:02:52 -0300

    Adicionar exercícios

--- logica-js-curso-aula2/app.js
+++ logica-js-curso-aula2/app.js
@@ -1,19 +1,52 @@
 let numeroaleatorio = gerarNumeroAleatorio();
+let tentativa =1;
 
 function exibirTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTela('h1', 'Jogo do Número Secreto');
-exibirTela('p', 'Escolha um número entre 1 e 10');
-
-function verificarChute() {
-    let chute = document.querySelector('input').value;
-    console.log(chute == numeroaleatorio);
-
+function exibirMensagemInicial(){
+    exibirTela('h1', 'Jogo do Número Secreto');
+    exibirTela('p', 'Escolha um número entre 1 e 10');
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 +1);
+    return parseInt(Math.random() * 10 + 1);
 }
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    exibirMensagemInicial();
+    numeroaleatorio = gerarNumeroAleatorio();
+    limparCampo();
+    tentativa = 1;
+}
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroaleatorio){
+        exibirTela('h1','Acertou');
+        let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativa = `Você descobriu o número secreto com ${tentativa} ${palavraTentativa}!`;
+        exibirTela('p',mensagemTentativa)
+        document.getElementById('reiniciar').removeAttribute('disabled');     
+    } else {
+        if (chute > numeroaleatorio) {
+            exibirTela('p','O número é menor');
+        } else {
+            exibirTela('p','O número é maior');
+        }
+        tentativa++;
+        limparCampo();
+    }
+
+}
+
+exibirMensagemInicial();
+
--- logica-js-curso-aula2/exerciciosFuncao.js
+++ logica-js-curso-aula2/exerciciosFuncao.js
@@ -1,45 +1,83 @@
-//1-Criar uma função que exibe "Olá, mundo!" no console.
-function exibirConsole(){
-    console.log('Olá Mundo');
+// //1-Criar uma função que exibe "Olá, mundo!" no console.
+// function exibirConsole(){
+//     console.log('Olá Mundo');
+// }
+
+// exibirConsole();
+
+// //2-Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+// function exibirNome(nome){
+//     console.log(`Olá, ${nome}`);
+// }
+
+// exibirNome('Miguel');
+
+// //3-Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+// function calculaDobro(numero){
+//     return numero * 2;    
+// }
+
+// let resultado = calculaDobro('3');
+// console.log(resultado);
+
+// //4-Criar uma função que recebe três números como parâmetros e retorna a média deles.
+// function media(numero1,numero2,numero3){
+//     return parseInt((numero1 + numero2 + numero3) / 3);
+// }
+
+// let resultadoMedia = media(20,20,20);
+// console.log(resultadoMedia);
+
+// //5-Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+// function econtraMaior(a,b){
+//     return a > b ? a : b;  
+// }
+
+// let maiorNumero = econtraMaior(10,100);
+// console.log(maiorNumero);
+
+// //6-Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
+// function multiplicaNumero(x){
+//     return x * x;
+// }
+
+// let resultadoMutiplicacao = multiplicaNumero(10);
+// console.log(resultadoMutiplicacao);
+
+// //3.1-Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+
+// function calculaIMC(altura,peso){
+//     let imc = parseInt(peso / (altura * altura));
+//     console.log(imc);
+// }
+
+// calculaIMC(1.70, 70);
+
+//3.2-Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+
+function calculafatorial(numero){
+    if (numero < 0){
+
+        console.log('Fatorial não definido');
+    } else{
+        let resultado = 1;
+        for (let contador = 1; contador <= numero ; contador++){
+        resultado*= contador;
+        }
+    return(resultado);
+    }
 }
 
-exibirConsole();
+calculafatorial(5);
+let paragrafo =document.querySelector('p','Teste');
 
-//2-Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
-function exibirNome(nome){
-    console.log(`Olá, ${nome}`);
-}
+//3.3-Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
 
-exibirNome('Miguel');
 
-//3-Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
-function calculaDobro(numero){
-    return numero * 2;    
-}
 
-let resultado = calculaDobro('3');
-console.log(resultado);
+//3.4-Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
 
-//4-Criar uma função que recebe três números como parâmetros e retorna a média deles.
-function media(numero1,numero2,numero3){
-    return parseInt((numero1 + numero2 + numero3) / 3);
-}
+//3.5-Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
 
-let resultadoMedia = media(20,20,20);
-console.log(resultadoMedia);
+//3.7-Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
 
-//5-Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
-function econtraMaior(a,b){
-    return a > b ? a : b;  
-}
-
-let maiorNumero = econtraMaior(10,100);
-console.log(maiorNumero);
-
-//6-Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
-function multiplicaNumero(x){
-    return x * x;
-}
-
-let resultadoMutiplicacao = multiplicaNumero(10);
-console.log(resultadoMutiplicacao);
